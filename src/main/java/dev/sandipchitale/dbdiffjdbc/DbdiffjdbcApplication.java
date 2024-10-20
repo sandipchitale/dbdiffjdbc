db74df189a047c387890159a132eaeece5f04572
Sandip Chitale <sandipchitale@gmail.com>
2024-10-20 15:16:54 -0700

    STart diff impl.

--- src/main/java/dev/sandipchitale/dbdiffjdbc/DbdiffjdbcApplication.java
+++ src/main/java/dev/sandipchitale/dbdiffjdbc/DbdiffjdbcApplication.java
@@ -10,6 +10,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.util.DigestUtils;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.stream.StreamSupport;
 
 @SpringBootApplication
 public class DbdiffjdbcApplication {
@@ -29,7 +36,6 @@ public class DbdiffjdbcApplication {
 
             namesToDiff1Repository.findAllIncludeRowId().forEach((NamesToDiff1WithCtId namesToDiff1WithCtId) -> {
                 namesToDiff1Repository.findByCtId(String.valueOf(namesToDiff1WithCtId.ctid())).ifPresent(System.out::println);
-//                System.out.println(namesToDiff1WithCtId);
             });
 
             NamesToDiff2 namesToDiff2 = new NamesToDiff2(null, "Jay", 28);
@@ -40,8 +46,43 @@ public class DbdiffjdbcApplication {
 
             namesToDiff2Repository.findAllIncludeRowId().forEach((NamesToDiff2WithCtId namesToDiff2WithCtId) -> {
                 namesToDiff2Repository.findByCtId(String.valueOf(namesToDiff2WithCtId.ctid())).ifPresent(System.out::println);
-//                System.out.println(namesToDiff2WithCtId);
             });
+
+//            Map<String, Object> namesToDiff1DigestsToRowIdMap = new TreeMap<>();
+//            Map<String, Object> namesToDiff2DigestsToRowIdMap = new TreeMap<>();
+//
+//            StreamSupport.stream(namesToDiff1Repository.findAllIncludeRowId().spliterator(), false).forEach((NamesToDiff1WithCtId namesToDiff1WithCtId) -> {
+//                namesToDiff1DigestsToRowIdMap.put(DigestUtils.md5DigestAsHex(namesToDiff1WithCtId.toBytes()), namesToDiff1WithCtId.ctid());
+//            });
+//
+//            StreamSupport.stream(namesToDiff2Repository.findAllIncludeRowId().spliterator(), false).forEach((NamesToDiff2WithCtId namesToDiff2WithCtId) -> {
+//                namesToDiff2DigestsToRowIdMap.put(DigestUtils.md5DigestAsHex(namesToDiff2WithCtId.toBytes()), namesToDiff2WithCtId.ctid());
+//            });
+//
+//            Set<String> namesToDiff1DigestsSet = new TreeSet<>(namesToDiff1DigestsToRowIdMap.keySet());
+//            namesToDiff1DigestsSet.retainAll(namesToDiff2DigestsToRowIdMap.keySet());
+//            System.out.println("In both NamesToDiff1 and NamesToDiff2: ");
+//
+//            namesToDiff1DigestsSet.forEach((String key) -> {
+//                Object ctId = namesToDiff1DigestsToRowIdMap.get(key);
+//                namesToDiff1Repository.findByCtId(String.valueOf(ctId)).ifPresent(System.out::println);
+//            });
+//
+//            namesToDiff1DigestsSet = new TreeSet<>(namesToDiff1DigestsToRowIdMap.keySet());
+//            namesToDiff1DigestsSet.removeAll(namesToDiff2DigestsToRowIdMap.keySet());
+//            System.out.println("In NamesToDiff1 only: ");
+//            namesToDiff1DigestsSet.forEach((String key) -> {
+//                Object ctId = namesToDiff1DigestsToRowIdMap.get(key);
+//                namesToDiff1Repository.findByCtId(String.valueOf(ctId)).ifPresent(System.out::println);
+//            });
+//
+//            Set<String> namesToDiff2DigestsSet = new TreeSet<>(namesToDiff2DigestsToRowIdMap.keySet());
+//            namesToDiff2DigestsSet.removeAll(namesToDiff1DigestsToRowIdMap.keySet());
+//            System.out.println("In NamesToDiff2 only: ");
+//            namesToDiff2DigestsSet.forEach((String key) -> {
+//                Object ctId = namesToDiff2DigestsToRowIdMap.get(key);
+//                namesToDiff1Repository.findByCtId(String.valueOf(ctId)).ifPresent(System.out::println);
+//            });
         };
     }
 }
--- src/main/java/dev/sandipchitale/dbdiffjdbc/postgres2/entities/NamesToDiff2WithCtId.java
+++ src/main/java/dev/sandipchitale/dbdiffjdbc/postgres2/entities/NamesToDiff2WithCtId.java
@@ -3,6 +3,12 @@ package dev.sandipchitale.dbdiffjdbc.postgres2.entities;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
+import java.nio.charset.StandardCharsets;
+
 @Table(name = "names")
 public record NamesToDiff2WithCtId(@Id Long id, String name, int age, Object ctid) {
+    // Used to calculate digest
+    public byte[] toBytes() {
+        return String.format("%d|%s|%d", id, name, age).getBytes(StandardCharsets.UTF_8);
+    }
 }
